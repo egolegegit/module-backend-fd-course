4bcd6ef7874316ae3d66e4eea553dcdfb3be4aa9
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-01 19:37:51 +0700

    feat(project): init

---
+++ index.js
@@ -0,0 +1,45 @@
+﻿const yargs = require("yargs");
+const pkg = require("./package.json");
+const { addNote, removeNote, printNotes } = require("./notes.controller");
+
+yargs.version(pkg.version);
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  async handler({ title }) {
+    await addNote(title);
+  },
+});
+
+yargs.command({
+  command: "del",
+  describe: "Delete note from list",
+  builder: {
+    id: {
+      type: "string",
+      describe: "Delete id",
+      demandOption: true,
+    },
+  },
+  async handler({ id }) {
+    await removeNote(id);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    await printNotes();
+  },
+});
+
+yargs.parse();
---
+++ notes.controller.js
@@ -0,0 +1,41 @@
+﻿const fs = require("fs/promises");
+const path = require("path");
+const chalk = require("chalk");
+
+const notesPath = path.join(__dirname, "db.json");
+
+async function addNote(title) {
+  const notes = await getNotes();
+  const note = { title, id: Date.now().toString() };
+  notes.push(note);
+
+  await fs.writeFile(notesPath, JSON.stringify(notes));
+  console.log(chalk.green.inverse("Note was added!"));
+}
+
+async function removeNote(id) {
+  const notes = await getNotes();
+  const result = notes.filter((item) => item.id !== id);
+
+  await fs.writeFile(notesPath, JSON.stringify(result));
+  console.log(chalk.red(`note with id: ${id} deleted!`));
+}
+
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, { encoding: "utf-8" });
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : [];
+}
+
+async function printNotes() {
+  const notes = await getNotes();
+  console.log(chalk.bgGray("List notes:"));
+  notes.forEach((note) => {
+    console.log(`${chalk.green(`id: ${note.id}`)} - ${chalk.blue(note.title)}`);
+  });
+}
+
+module.exports = {
+  addNote,
+  removeNote,
+  printNotes,
+};
