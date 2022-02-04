aabd4b6826496277390ba91b4e05391c01cbf61f
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-04 15:40:36 +0700

    feat(project):
    5. Работа с Express.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 ﻿const http = require("http");
+const express = require("express");
 const chalk = require("chalk");
 const fs = require("fs/promises");
 const path = require("path");
@@ -7,28 +8,18 @@ const { addNote } = require("./notes.controller");
 const port = 3000;
 const basePath = path.join(__dirname, "pages");
 
-const server = http.createServer(async (req, res) => {
-  if (req.method === "GET") {
-    const content = await fs.readFile(path.join(basePath, "index.html"));
-    res.writeHead(200, { "Content-type": "text/html" });
-    res.end(content);
-  } else if (req.method === "POST") {
-    const body = [];
-    res.writeHead(200, { "Content-type": "text/plain; charset=utf-8" });
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-    req.on("data", (data) => {
-      body.push(Buffer.from(data).toString("utf8"));
-    });
-
-    req.on("end", () => {
-      console.log(body.toString());
-      const title = body.toString().split("=")[1].replaceAll("+", " ");
-      addNote(title);
-      res.end(`Title= ${title}`);
-    });
-  }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(basePath, "index.html"));
 });
 
-server.listen(port, () => {
+app.post("/", async (req, res) => {
+  await addNote(req.body.title);
+  res.sendFile(path.join(basePath, "index.html"));
+});
+
+app.listen(port, () => {
   console.log(chalk.green(`Server has been started on port ${port}...`));
 });
---
+++ server.example.js
@@ -0,0 +1,26 @@
+﻿import http from "http";
+import fs from "fs/promises";
+import path from "path";
+import { addNote } from "./notes.controller";
+
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET") {
+    const content = await fs.readFile(path.join(basePath, "index.html"));
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(content);
+  } else if (req.method === "POST") {
+    const body = [];
+    res.writeHead(200, { "Content-type": "text/plain; charset=utf-8" });
+
+    req.on("data", (data) => {
+      body.push(Buffer.from(data).toString("utf8"));
+    });
+
+    req.on("end", () => {
+      console.log(body.toString());
+      const title = body.toString().split("=")[1].replaceAll("+", " ");
+      addNote(title);
+      res.end(`Title= ${title}`);
+    });
+  }
+});
