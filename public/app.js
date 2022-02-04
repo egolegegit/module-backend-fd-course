b193d7acfc1e946d7a96029540409807e596f02f
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-04 23:44:24 +0700

    feat(project):
    Практическое задание #1
    fix

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
-﻿console.log("Client script");
-document.addEventListener("click", (event) => {
+﻿document.addEventListener("click", (event) => {
   if (event.target.dataset.type === "remove") {
     const id = event.target.dataset.id;
 
@@ -11,7 +10,11 @@ document.addEventListener("click", (event) => {
   if (event.target.dataset.type === "edit") {
     const id = event.target.dataset.id;
 
-    edit(id);
+    editNote(id).then(({ status, title }) => {
+      if (status === 200) {
+        event.target.closest("li").firstElementChild.innerHTML = title;
+      }
+    });
   }
 });
 
@@ -19,19 +22,16 @@ async function remove(id) {
   fetch(`/${id}`, { method: "DELETE" });
 }
 
-async function edit(id) {
-  let content = prompt("Введите новое значение");
-  if (content) {
-    content = content.trim();
+async function editNote(id) {
+  let title = prompt("Введите новое значение");
+  if (title) {
+    title = title.trim();
     const res = await fetch(`/${id}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title: content }),
+      body: JSON.stringify({ title: title }),
     });
 
-    if (res.status === 200) {
-      const title = document.querySelector(`[data-id="${id}"]`);
-      title.innerHTML = content;
-    }
+    return { status: res.status, title: title };
   }
 }
