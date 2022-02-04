acd5b531bc37ae2db4b7b2c0d3a82a55012733f0
Рыбников Андрей Анатольевич <43681241+egolegegit4226@users.noreply.github.com>
2022-02-04 14:34:36 +0700

    feat(project):
    4. Обработка POST запроса

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const chalk = require("chalk");
 const fs = require("fs/promises");
 const path = require("path");
+const { addNote } = require("./notes.controller");
 
 const port = 3000;
 const basePath = path.join(__dirname, "pages");
@@ -9,9 +10,22 @@ const basePath = path.join(__dirname, "pages");
 const server = http.createServer(async (req, res) => {
   if (req.method === "GET") {
     const content = await fs.readFile(path.join(basePath, "index.html"));
-    // res.setHeader("Content-type", "text/html");
     res.writeHead(200, { "Content-type": "text/html" });
     res.end(content);
+  } else if (req.method === "POST") {
+    const body = [];
+    res.writeHead(200, { "Content-type": "text/plain; charset=utf-8" });
+
+    req.on("data", (data) => {
+      body.push(Buffer.from(data).toString("utf8"));
+    });
+
+    req.on("end", () => {
+      console.log(body.toString());
+      const title = body.toString().split("=")[1].replaceAll("+", " ");
+      addNote(title);
+      res.end(`Title= ${title}`);
+    });
   }
 });
 
